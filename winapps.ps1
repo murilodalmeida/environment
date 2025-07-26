a0e6068d3738ba58acf5340b7a8c823c9eadac17
Murilo A <8878423+murilodalmeida@users.noreply.github.com>
2025-07-26 14:36:38 +0100

    scripts/winapps
    Added winapps.ps1 file with the minimum list of apps to be installed.

---
+++ winapps.ps1
@@ -0,0 +1,35 @@
+# Installs Windows Apps
+
+# --- Update Source ---
+Write-Host "Updating winget sources..." -ForegroundColor Cyan
+winget source update
+
+# --- Apps list with flags ---
+$apps = @(
+    "Microsoft.Edit -h"
+    "Microsoft.VisualStudioCode -h"
+    "Microsoft.PowerToys -h"
+    "Microsoft.DotNet.SDK.9 -h"
+    "Mozilla.Firefox -h"
+    "IDRIX.VeraCrypt -h"
+    "7zip.7zip -h"
+    "Parsec.Parsec -h"
+    "OpenJS.NodeJS.LTS -h"
+    "Genymobile.scrcpy -h"
+    "GnuWin32.Make -h"
+    "Stremio.Stremio -h"
+    "Git.Git -h"
+    "Discord.Discord -h"
+    "Valve.Steam -i"
+    "JetBrains.Rider -i"
+)
+
+Write-Host "`nInstalling apps..." -ForegroundColor Green
+foreach ($app in $apps) {
+    $parts = $app -split " "
+    $package = $parts[0]
+    $flag = $parts[1]
+
+    Write-Host "Installing $package..." -ForegroundColor Yellow
+    winget install -e $flag --id $package
+}
